58cd72f4363bb63d2b7ea0d063b12a52266bcbde
James Montalvo <edwin.j.montalvo@nasa.gov>
2016-11-30 19:39:52 -0600

    add json, intl, pspell back in

--- scripts/php.sh
+++ scripts/php.sh
@@ -16,26 +16,24 @@ yum -y install https://centos7.iuscommunity.org/ius-release.rpm
 # not available (yet?) with PHP 7.0:
 #	php70u-pecl-memcache \ (required for SAML auth)
 #		https://github.com/iuscommunity/wishlist/issues/38
-#	php70u-pspell \ (I don't think this is required for anything)
-# 	php70u-pecl-jsonc \ (may not be required anymore due to JSON module
-#		inclusion in PHP7, ref:
-#		https://github.com/iuscommunity/wishlist/issues/87)
-# 	php70u-intl \ (Not entirely sure we're actually using this)
 yum install -y \
 	php70u \
 	php70u-cli \
 	php70u-common \
 	php70u-devel \
 	php70u-gd \
+	php70u-pspell \
 	php70u-snmp \
 	php70u-xml \
 	php70u-xmlrpc \
 	php70u-mysqlnd \
+	php70u-json \
 	php70u-pdo \
 	php70u-odbc \
 	php70u-pear \
 	php70u-process \
 	php70u-bcmath \
+	php70u-intl \
 	php70u-opcache \
 	php70u-soap \
 	php70u-mbstring \
